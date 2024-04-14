f6008cc23974bb855bae759921344b76e916fcf5
slowrain123 <166286907+slowrain123@users.noreply.github.com>
2024-04-14 12:27:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from random import*
 clock = time.Clock()
 window = display.set_mode((700,500))
 display.set_caption("Догонялки")
-background = transform.scale(image.load("gg.png"),(700,500))
+background = transform.scale(image.load("gg.jpg"),(700,500))
 class GameSPrite(sprite.Sprite):
-        def __init__(self, player_image,player_x,player_y,player_speed):
+        def __init__(self, player_image,player_x,player_y,player_speed,player_asd,player_hh):
             super().__init__()
-            self.image = transform.scale(image.load(player_image),(15,70))
+            self.image = transform.scale(image.load(player_image),(player_asd,player_hh))
             self.speed = player_speed
             self.rect = self.image.get_rect()
             self.rect.x = player_x
@@ -41,16 +41,48 @@ class Enemy(GameSPrite):
         if keys_pressed[K_RIGHT]:
             self.rect.x += self.speed
 
-sprite1 = Player("ll.jpg",300,50,5)
-sprite2 = Enemy("rr.webp",300,50,5)
 
+class Ball(GameSPrite):
+    directionX = 'left'
+    directionY = "up"
+    def update(self):
+        if self.rect.x <= 0:
+            self.directionX = "right"
+        if self.rect.x >= 700:
+            self.directionX = "left"
+        if self.directionX == "left":
+            self.rect.x -= self.speed
+        if self.directionX == "right":
+            self.rect.x += self.speed
+
+
+        if self.rect.y <= 0:
+            self.directionY = "down"
+        if self.rect.y >= 450:
+            self.directionY = "up"
+        if self.directionY == "up":
+            self.rect.y -= self.speed
+        if self.directionY == "down":
+            self.rect.y += self.speed
+            
+
+
+sprite1 = Player("gg.png",300,50,5, 70,50)
+sprite2 = Enemy("ll.webp",300,50,5, 70,50)
+sprite3 = Ball("tt.png",50,50,5,70,50)
 finish = False
 game = True
 while game:
     if finish!= True:
         window.blit(background,(0,0))
-    
-   
+        sprite1.reset()
+        sprite1.update()
+        sprite2.reset()
+        sprite2.update()
+        sprite3.reset()
+        sprite3.update()
+        display.update()
+
 
 
     def reset(self):
@@ -62,9 +94,6 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        sprite1.reset()
-        sprite1.update()
-        sprite2.reset()
-        sprite2.update()
-        display.update()
-        clock.tick(300)
+        
+    clock.tick(60)
+
